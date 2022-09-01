7bba66ac4c1f0b3a0f8ae1746cde237ede439ccd
Leandro Morillo <leandro.m9601@gmail.com>
2022-08-31 20:51:32 -0500

    conjuntos

---
+++ conjuntos.js
@@ -0,0 +1,10 @@
+const familia = ["Leandro", "Cecilia", "Helio", "Juan", "Jesus"];
+const familySet = new Set(familia);
+
+// Añadir mi nombre duplicado con set
+familySet.add("Leandro");
+console.log(familySet);
+
+// Añadir a mi Padre JavaScript
+familySet.add("JavaScript");
+console.log(familySet);
